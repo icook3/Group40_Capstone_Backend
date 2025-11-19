a222b866cd31aabcad75e8bf91e519d50bde630c
legowiifun <arath5@gmail.com>
2025-11-19 09:06:17 -0700

    Change the default path for the peerServer

--- peer_service/src/peerServer.js
+++ peer_service/src/peerServer.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 import { PeerServer } from "peer";
 
 dotenv.config();
-const peerServer = PeerServer({port: process.env.PORT||9000, path: process.env.SERVERPATH||"/zlowServer", allow_discovery: true, corsOptions: [process.env.FRONT||"localhost:8000"]});
+const peerServer = PeerServer({port: process.env.PORT||9000, path: process.env.SERVERPATH||"/peerServer", allow_discovery: true, corsOptions: [process.env.FRONT||"localhost:8000"]});
